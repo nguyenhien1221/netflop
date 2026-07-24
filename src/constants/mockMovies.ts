43245bcba3497e3b8b901b4eb2e46d17b0a4d993
hiennguyen2904 <hiennm@iceteasoftware.com>
2026-07-24 11:32:00 +0700

    fix: build

--- src/constants/mockMovies.ts
+++ src/constants/mockMovies.ts
@@ -3,15 +3,210 @@ import type { IMovie } from "@/types/movie";
 export const featuredMovie: IMovie = {
   id: 161,
   title: "Dune: Part Two",
-  thumbnailUrl:
-    "https://m.media-amazon.com/images/M/MV5BNTc0YmQxMjEtODI5MC00NjFiLTlkMWUtOGQ5NjFmYWUyZGJhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
   description:
     "Paul Atreides unites with the Fremen while on a warpath of revenge against the conspirators who destroyed his family.",
-  category: "Sci-Fi",
-  price: 5,
-  currency: "ETH",
+  thumbnailUrl:
+    "https://m.media-amazon.com/images/M/MV5BNTc0YmQxMjEtODI5MC00NjFiLTlkMWUtOGQ5NjFmYWUyZGJhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
   videoUrl:
     "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
   isPremium: true,
+  price: 5,
+  currency: "ETH",
+  category: "Sci-Fi",
   hasPurchased: false,
 };
+
+export const newUpdatesMovies: IMovie[] = [
+  featuredMovie,
+  {
+    id: 162,
+    title: "Oppenheimer",
+    description:
+      "The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.",
+    thumbnailUrl: "https://i.ebayimg.com/images/g/aH4AAOSwXM1maWv7/s-l1200.jpg",
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Biography",
+    hasPurchased: false,
+  },
+  {
+    id: 163,
+    title: "Poor Things",
+    description:
+      "The incredible tale about the fantastical evolution of Bella Baxter, a young woman brought back to life by a brilliant and unorthodox scientist.",
+    thumbnailUrl:
+      "https://m.media-amazon.com/images/M/MV5BYWU2MjRjZTYtMjVkMS00MTBjLWFiMTAtYmZlYTk1YjkyMWFkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Comedy",
+    hasPurchased: false,
+  },
+  {
+    id: 164,
+    title: "The Batman",
+    description:
+      "When a sadistic serial killer begins murdering key political figures in Gotham, Batman is forced to investigate the city's hidden corruption.",
+    thumbnailUrl:
+      "https://m.media-amazon.com/images/M/MV5BMmU5NGJlMzAtMGNmOC00YjJjLTgyMzUtNjAyYmE4Njg5YWMyXkEyXkFqcGc@._V1_.jpg",
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Action",
+    hasPurchased: false,
+  },
+  {
+    id: 165,
+    title: "Everything Everywhere All at Once",
+    description:
+      "A middle-aged Chinese immigrant is swept up in an insane adventure in which she alone can save existence by exploring other universes.",
+    thumbnailUrl:
+      "https://m.media-amazon.com/images/M/MV5BOWNmMzAzZmQtNDQ1NC00Nzk5LTkyMmUtNGI2N2NkOWM4MzEyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Action",
+    hasPurchased: false,
+  },
+];
+
+export const continueWatchingMovies: IMovie[] = [
+  {
+    id: 167,
+    title: "Blade Runner 2049",
+    description:
+      "Young blade runner K's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard.",
+    thumbnailUrl:
+      "https://upload.wikimedia.org/wikipedia/vi/8/8b/Blade_Runner_2049_poster.jpg",
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Sci-Fi",
+    hasPurchased: false,
+  },
+  {
+    id: 168,
+    title: "Interstellar",
+    description:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    thumbnailUrl:
+      "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Sci-Fi",
+    hasPurchased: false,
+  },
+  {
+    id: 169,
+    title: "Inception",
+    description:
+      "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into a CEO's mind.",
+    thumbnailUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/71uKM+LdgFL.jpg",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Action",
+    hasPurchased: false,
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+  },
+  {
+    id: 169,
+    title: "Inception",
+    description:
+      "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into a CEO's mind.",
+    thumbnailUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/71uKM+LdgFL.jpg",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Action",
+    hasPurchased: false,
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+  },
+  {
+    id: 169,
+    title: "Inception",
+    description:
+      "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into a CEO's mind.",
+    thumbnailUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/71uKM+LdgFL.jpg",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Action",
+    hasPurchased: false,
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+  },
+];
+
+export const trendingMovies: IMovie[] = [
+  {
+    id: 169,
+    title: "Inception",
+    description:
+      "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into a CEO's mind.",
+    thumbnailUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/71uKM+LdgFL.jpg",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Action",
+    hasPurchased: false,
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+  },
+];
+
+export const actionMovies: IMovie[] = [
+  {
+    id: 169,
+    title: "Inception",
+    description:
+      "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into a CEO's mind.",
+    thumbnailUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/71uKM+LdgFL.jpg",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Action",
+    hasPurchased: false,
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+  },
+];
+
+export const sciFiMovies: IMovie[] = [
+  {
+    id: 169,
+    title: "Inception",
+    description:
+      "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into a CEO's mind.",
+    thumbnailUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/71uKM+LdgFL.jpg",
+    isPremium: true,
+    price: 5,
+    currency: "ETH",
+    category: "Action",
+    hasPurchased: false,
+    videoUrl:
+      "https://embed2.streamc.xyz/embed.php?hash=294db7106f9e9a9634b0b9230fecd440",
+  },
+];
